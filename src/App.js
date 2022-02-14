e01c55fe5e4784c467eebb24e62652607609c819
RitaCavalheiro <44837728+RitaCavalheiro@users.noreply.github.com>
2022-02-14 18:28:52 +0000

    integrated star background and new font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div class="page-bg"></div>
+
+      <div class="animation-wrapper">
+        <div class="particle particle-1"></div>
+        <div class="particle particle-2"></div>
+        <div class="particle particle-3"></div>
+        <div class="particle particle-4"></div>
+      </div>
       <header>
         <h1>Be a sesquipedalian</h1>
       </header>
